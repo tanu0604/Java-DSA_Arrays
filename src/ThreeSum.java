7cfc13239cf2292dccf603f72159525b2e6c819e
Tanushree Paul <125464289+tanu0604@users.noreply.github.com>
2024-03-12 21:52:48 +0530

    Add files via upload

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,27 +1,59 @@
-import java.util.Arrays;
-
+import java.util.*;
 public class ThreeSum {
-    public static void main(String[] args) {
-        int a[]={-1,-1,2,0,1};
-        int sum=0;
-        int[] threeSum=sum(a);
-        System.out.println((Arrays.toString(threeSum)));
-    }
-    static int[] sum(int[] a)
-    {
-        int sum=0;
-        for (int i=0;i<a.length;i++)
+    public static List< List < Integer > > triplet(int n, int []nums) {
+        // Write your code here.
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++)
         {
-            for (int j=i+1;i<a.length;i++)
+            if(i!=0 && nums[i]==nums[i-1])
             {
-                for (int k=i+2;i< a.length;i++)
+                continue;
+            }
+            int j=i+1;
+            int k=n-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0)
                 {
-                    sum=a[i]+a[j]+a[k];
-                    if(sum==0)
-                        return new int[]{a[i],a[j],a[k]};
+                    k--;
+                }
+                else if(sum<0)
+                {
+                    j++;
+                }
+                else{
+                    List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    //Skip the duplicate elements
+                    while(j<k && nums[j]==nums[j-1])
+                    {
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k+1])
+                    {
+                        k--;
+                    }
                 }
             }
         }
-        return new int[]{-1,-1,-1};
+        return ans;
+    }
+        public static void main(String[] args) {
+        int nums[]={-1,-1,2,0,1};
+        int n = nums.length;
+        List<List<Integer>> ans = triplet(n, nums);
+        for (List<Integer> it : ans) {
+            System.out.print("[");
+            for (Integer i : it) {
+                System.out.print(i + " ");
+            }
+            System.out.print("] ");
+        }
+        System.out.println();
     }
 }
